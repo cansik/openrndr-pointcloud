58058d953dff703501985912054d1bb07b17c8e3
Florian <florian@bildspur.ch>
2019-07-22 00:23:52 +0200

    added 1mio points

--- src/main/kotlin/ch/bildspur/pcl/SimpleTest.kt
+++ src/main/kotlin/ch/bildspur/pcl/SimpleTest.kt
@@ -7,10 +7,14 @@ import org.openrndr.math.Vector3
 import org.openrndr.math.transforms.transform
 
 fun main(args: Array<String>) {
+    var numFrames = 0
+    var pointCount = 1000 * 1000
+
     application {
         configure {
             width = 770
             height = 578
+            title = "PCL"
         }
         program {
             // -- create the vertex buffer
@@ -29,24 +33,27 @@ fun main(args: Array<String>) {
             // -- create the secondary vertex buffer, which will hold transformations
             val transforms = vertexBuffer(vertexFormat {
                 attribute("transform", VertexElementType.MATRIX44_FLOAT32)
-            }, 1000)
+            }, pointCount)
 
             // -- fill the transform buffer
             transforms.put {
-                for (i in 0 until 1000) {
+                for (i in 0 until pointCount) {
                     write(transform {
                         translate(Math.random() * width, Math.random() * height)
                         //rotate(Vector3.UNIT_Z, Math.random() * 360.0)
-                        scale(Math.random() * 30.0)
+                        //scale(Math.random() * 1.0)
                     })
                 }
             }
             extend {
+                application.windowTitle = "FPS: ${numFrames / seconds}"
+                numFrames++
+
                 drawer.fill = ColorRGBa.WHITE.opacify(0.25)
                 drawer.shadeStyle = shadeStyle {
                     vertexTransform = "x_viewMatrix = x_viewMatrix * i_transform;"
                 }
-                drawer.vertexBufferInstances(listOf(geometry), listOf(transforms), DrawPrimitive.TRIANGLE_STRIP, 1000)
+                drawer.vertexBufferInstances(listOf(geometry), listOf(transforms), DrawPrimitive.TRIANGLE_STRIP, pointCount)
             }
         }
     }
